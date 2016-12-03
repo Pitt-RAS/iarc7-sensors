c83e45e013473935cdd644ba2cfd261a10e1df88
dmgilmour <dmgilmour@pitt.edu>
2016-12-02 19:23:46 -0500

    Fix stupid do while mistake

--- src/altimeter.cpp
+++ src/altimeter.cpp
@@ -16,6 +16,10 @@ int main(int argc, char **argv) {
 
     ros::init(argc, argv, "altimeter");
     ros::NodeHandle n;
+
+    std::string altitude_frame;
+    n.param("output_frame", altitude_frame, std::string("altimeter_frame"));
+
     ros::Publisher altitude_pub = n.advertise<iarc7_msgs::Float64Stamped>("altitude", 0);
     ros::Publisher velocity_pub = n.advertise<iarc7_msgs::Float64Stamped>("velocity", 0);
 
@@ -24,23 +28,30 @@ int main(int argc, char **argv) {
     
     bool err;
     do {
+        ros::spinOnce();
         err = lidarLite.openLidarLite();
+        if (err == false) {
+            ROS_ERROR("Unable to connect to LidarLite v2, trying to connect");
+        }
     } while (err == false);
 
     
     while(ros::ok() && lidarLite.error >= 0){
+        ros::spinOnce();
         double altitude = (lidarLite.getDistance()) / 100.0;
         double velocity = (lidarLite.getVelocity()) / 100.0;
         // altitude_msg.data = altitude;
         
-        if (altitude >= 0 && velocity >= 0) {
+        if (altitude >= 0) {
             iarc7_msgs::Float64Stamped altitude_msg;
             iarc7_msgs::Float64Stamped velocity_msg;
             ros::Time tempTime = ros::Time::now();
-            filter.updateFilter(altitude, velocity, tempTime);
+//            filter.updateFilter(altitude, velocity, tempTime);
+            altitude_msg.header.frame_id = altitude_frame;
             altitude_msg.header.stamp = tempTime;
+            velocity_msg.header.frame_id = altitude_frame;
             velocity_msg.header.stamp = tempTime;
-            altitude = filter.getFilteredAltitude(tempTime);
+//            altitude = filter.getFilteredAltitude(tempTime);
             altitude_msg.data = altitude; 
             velocity_msg.data = velocity;
             altitude_pub.publish(altitude_msg);
@@ -48,9 +59,12 @@ int main(int argc, char **argv) {
         }
         else {
             do {
+                ros::spinOnce();
+                ROS_ERROR("Lost connection with LidarLite v2, trying to connect");
                 lidarLite.closeLidarLite();
                 err = lidarLite.openLidarLite();
             } while (err == false);
+            ROS_ERROR("Successfully established connection with LidarLite v2"); 
         }
         
     }
