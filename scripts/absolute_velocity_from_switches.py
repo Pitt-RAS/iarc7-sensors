45e3fba1d7cbfad55892d036052e8ba71c424ff7
Levi Burner <leviburner@gmail.com>
2017-06-09 15:33:06 -0400

    Wait to publish before connecting with safety node. Check for valid update rate

--- scripts/absolute_velocity_from_switches.py
+++ scripts/absolute_velocity_from_switches.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 import rospy
 
 from iarc7_msgs.msg import LandingGearContactsStamped
@@ -13,18 +14,37 @@ def callback(msg):
         out_msg.header.frame_id = 'map'
         out_msg.header.stamp = msg.header.stamp
         pub.publish(out_msg)
+    global last_msg_stamp
+    last_msg_stamp = msg.header.stamp
+
+last_msg_stamp = None
 
 if __name__ == '__main__':
     rospy.init_node('absolute_vel_from_switches')
     rospy.Subscriber('landing_gear_contact_switches', LandingGearContactsStamped, callback)
     pub = rospy.Publisher('absolute_vel', TwistWithCovarianceStamped, queue_size=0)
 
+    # Wait for a valid timestamp
+    while rospy.Time.now() == rospy.Time(0):
+        if rospy.is_shutdown():
+            sys.exit()  
+
+    # Make sure a message is recieved and published before connecting
+    # to safety
+    start_time = rospy.Time.now()
+    while True:
+        assert((rospy.Time.now() - start_time) < rospy.Duration(10.0))
+        if last_msg_stamp is not None:
+            break
+
     safety_client = SafetyClient('absolute_switch velocity_publisher')
     assert(safety_client.form_bond())
 
     while not rospy.is_shutdown():
-        if (self._safety_client.is_fatal_active()
-           or self._safety_client.is_safety_active()):
+        if (safety_client.is_fatal_active()
+           or safety_client.is_safety_active()):
             # We don't have a safety response for this node so just exit
-           break
-
+            break
+        # Make sure we are within  10Hz of the 50Hz target update rate
+        if rospy.Time.now() - last_msg_stamp > rospy.Duration(1.0/(50.0-10.0)):
+            rospy.logwarn('Absolute velocity from contact switches cant hit update rate')
