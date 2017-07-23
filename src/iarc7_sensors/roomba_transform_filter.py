c8e10d699a7cc8cc0f6e4863796ba2b1d52e7342
Elliot Miller <ejm4010@rit.edu>
2017-07-23 03:46:03 -0400

    Minimum viable roomba transform publisher

---
+++ src/iarc7_sensors/roomba_transform_filter.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+import rospy
+import tf2_ros
+
+from geometry_msgs.msg import TransformStamped
+from iarc7_msgs.msg import OdometryArray
+
+class RoombaTransformFilter(object):
+    def __init__(self):
+        rospy.init_node('roomba_transform_filter')
+
+        self._odometry_sub = rospy.Subscriber(
+                'roombas',
+                OdometryArray,
+                self._callback)
+
+        self._transform_broadcaster = tf2_ros.TransformBroadcaster()
+
+        try:
+            rospy.spin()
+        except KeyboardInterrupt:
+            rospy.loginfo("Shutting down roomba_transform_filter");
+
+    def _construct_transform(self, odom):
+        transform_msg = TransformStamped()
+
+        transform_msg.header.stamp = odom.header.stamp
+        transform_msg.header.frame_id = 'map'
+        transform_msg.child_frame_id = odom.child_frame_id + "/base_link"
+
+        transform_msg.transform.rotation = odom.pose.pose.orientation
+
+        transform_msg.transform.translation.x = odom.pose.pose.position.x
+        transform_msg.transform.translation.y = odom.pose.pose.position.y
+        transform_msg.transform.translation.z = 0
+
+        return transform_msg
+
+    def _callback(self, msg):
+        for odom in msg.data:
+            transform_msg = self._construct_transform(odom)
+            self._transform_broadcaster.sendTransform(transform_msg)
+
+if __name__ == '__main__':
+    RoombaTransformFilter()
