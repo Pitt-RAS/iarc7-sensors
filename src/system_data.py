805d666a5203c51d3891db270912469f4b61d4eb
NathanielTodd <nate4842@gmail.com>
2017-10-16 19:45:44 -0400

    syncing files

--- src/system_data.py
+++ src/system_data.py
@@ -4,31 +4,30 @@ import rospy
 import subprocess
 from std_msgs.msg import String
 
-def dataRead():
+def system_stat_monitor():
     
-    ps_pub = rospy.Publisher('ps_stats', String, queue_size=20)
-    top_pub = rospy.Publisher('top_stats', String, queue_size=20)
-    tegra_pub = rospy.Publisher('tegra_stats', String, queue_size=20)
-    rospy.init_node('dataRead')
+    rospy.init_node('system_stat_monitor')
 
+    #topic for string from ps_run
+    ps_pub = rospy.Publisher('ps_stats', String, queue_size=20)
+    #topic for string from top -bn1
+    top_pub = rospy.Publisher('top_stats', String, queue_size=20)
+    #topic for string from tegra.sh
+    tegra_pub = rospy.Publisher('tegra_stats', String, queue_size=20)
+    
     rate = rospy.Rate(10) #10 hz
     while not rospy.is_shutdown():
         
-
         output = subprocess.check_output(['top','-bn1'])
         top_pub.publish(output)
 
-        #need to run chmod +x so permission is not denied
-        subprocess.call(['chmod','+x','tegra.sh'])
         output = subprocess.check_output(['./tegra.sh'])
         tegra_pub.publish(output)
 
-        #need to run chmod +x so permission is not denied
-        subprocess.call(['chmod','+x','run.sh'])
-        output = subprocess.check_output(['./run.sh'])
+        output = subprocess.check_output(['./ps_run.sh'])
         ps_pub.publish(output)
 
         rate.sleep()
 
 if __name__ == '__main__':
-    dataRead()
+    system_stat_monitor()
