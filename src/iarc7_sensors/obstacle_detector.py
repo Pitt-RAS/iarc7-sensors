361acf1966f6b6a7e07d94a5b7ca21d4cec11881
Aaron Miller <acmiller273@gmail.com>
2017-12-21 15:46:36 -0500

    Added some comments and made things clearer

--- src/iarc7_sensors/obstacle_detector.py
+++ src/iarc7_sensors/obstacle_detector.py
@@ -19,7 +19,32 @@ from tf import transformations
 import tf2_ros
 import uuid
 
+'''
+Obstacle detector
+
+Detects 2d cylinders using a planar scanning LIDAR, rejecting points outside
+of a rectangular volume representing the bounds of the arena and inside of a
+cylinder representing the drone
+
+Expects incoming LaserScan messages on the `scan` topic, and publishes
+`ObstacleArray` messages on the `obstacles` topic and `MarkerArray` messages
+on the `obstacle_markers` topic
+'''
+
 def slerp(q0, q1, t):
+    '''
+    Slerp between quaternions q0 and q1 with samples at times t
+    Based on https://en.wikipedia.org/wiki/Slerp#Source_code
+
+    Args:
+        q0: start quaternion, sequence of length 4, of the form [x, y, z, w]
+        q1: end quaternion, sequence of length 4, of the form [x, y, z, w]
+        t: sequence of numbers between 0 and 1 representing times
+
+    Returns:
+        orientations: numpy array of shape (len(t), 4), representing the quaternion
+                      at each time in t
+    '''
     q0 = q0 / linalg.norm(q0)
     q1 = q1 / linalg.norm(q1)
 
@@ -45,24 +70,44 @@ def scan_callback(msg):
     else:
         rospy.logerr('Rejecting scan with old timestamp: %s'%msg.header.stamp)
 
-def get_circle(points, radius, drone_pos, settings):
+def get_best_circle_for_cluster(points, radius, drone_pos):
+    '''
+    Computes a best-fit circle for a given set of points in 2d
+
+    Args:
+        points: numpy array of shape (N, 2), locations of observed points
+        radius: radius of circle to fit
+        drone_pos: numpy array of shape (2), location of drone
+
+    Returns:
+        center: sequence of length 2, center of best-fit circle
+    '''
     assert points.size
 
+    # Calculate initial guess for the center, assuming the average of the
+    # points is the intersection of the line between the drone and obstacle
+    # centers and the bounding circle of the obstacle
     mean_point = np.mean(points, axis=0)
-    guess_pos_offset = mean_point - drone_pos
-    guess_pos_offset *= (
-            (linalg.norm(guess_pos_offset) + settings['obst_radius'])
-          / linalg.norm(guess_pos_offset)
-        )
-    guess_pos = drone_pos + guess_pos_offset
+    boundary_point_relative = mean_point - drone_pos
+    center_point_relative = (boundary_point_relative
+                           * (linalg.norm(boundary_point_relative) + radius)
+                           / linalg.norm(boundary_point_relative))
+    guess_pos = drone_pos + center_point_relative
 
     if points.shape[0] == 1:
+        # We only have one point, so the fitter won't do any better
         return guess_pos
     else:
         def f(x):
+            '''
+            Cost function, see documentation for scipy.optimize.leastsq
+            '''
             return linalg.norm(x - points, axis=1) - radius
 
         def Dfun(x):
+            '''
+            Jacobian of f
+            '''
             return ((x-points)
                   / np.expand_dims(linalg.norm(x - points, axis=1),
                                    axis=-1)).T
@@ -76,9 +121,29 @@ def get_circle(points, radius, drone_pos, settings):
         return x
 
 def get_rotation_matrices(orientation_start, orientation_end, samples):
+    '''
+    Computes a sequence of rotation matrices interpolating between initial
+    and final orientations
+
+    This is a vectorized version of the quaternion_matrix function
+    in transformations.py, which can be found online here:
+    https://www.lfd.uci.edu/~gohlke/code/transformations.py.html
+
+    Args:
+        orientation_start: start quaternion, sequence of length 4, of the
+                           form [x, y, z, w]
+        orientation_end: end quaternion, sequence of length 4, of the
+                         form [x, y, z, w]
+        samples: sequence of N numbers between 0 and 1 representing times
+
+    Returns:
+        numpy array of shape (N, 4, 4), representing a rotation (as an affine
+        transformation matrix) for each requested sample
+    '''
     assert orientation_start.shape == (4,)
     assert orientation_end.shape == (4,)
 
+    # Interpolated orientations as quaternions
     orientations = slerp(orientation_start,
                          orientation_end,
                          samples)
@@ -98,6 +163,7 @@ def get_rotation_matrices(orientation_start, orientation_end, samples):
 
     assert rotation_matrices.shape == (4, 4, len(samples))
 
+    # Reshape rotation_matrices
     rotation_matrices = np.einsum('ijt->tij', rotation_matrices)
 
     assert rotation_matrices.shape == (len(samples), 4, 4)
@@ -105,6 +171,18 @@ def get_rotation_matrices(orientation_start, orientation_end, samples):
     return rotation_matrices
 
 def get_transform_matrices(tf_start, tf_end, samples):
+    '''
+    Gets a sequence of transforms interpolating between tf_start and tf_end
+
+    Args:
+        tf_start: a TransformStamped representing the initial transform
+        tf_end: a TransformStamped representing the final transform
+        samples: a sequence of N numbers between 0 and 1 representing the
+                 times at which the interpolation is performed
+
+    Returns:
+        a numpy array of shape (N, 4, 4) representing N affine transformations
+    '''
     translation_start = np.array([tf_start.transform.translation.x,
                                   tf_start.transform.translation.y,
                                   tf_start.transform.translation.z])
@@ -131,27 +209,52 @@ def get_transform_matrices(tf_start, tf_end, samples):
     assert translation_matrices.shape == (len(samples), 4, 4)
     assert rotation_matrices.shape == (len(samples), 4, 4)
 
+    # Calculate translation * rotation for every timestep
     transform_matrices = np.einsum('tij,tjk->tik',
                                    translation_matrices,
                                    rotation_matrices)
     return transform_matrices
 
 def get_translation_matrices(translation_start, translation_end, samples):
+    '''
+    Gets a sequence of transformation matrices representing translations
+    interpolated between translation_start and translation_end
+
+    Args:
+        translation_start: a sequence of length 3 representing the initial
+                           translation
+        translation_end: a sequence of length 3 representing the final
+                           translation
+        samples: a sequence of N numbers between 0 and 1 representing the
+                 times at which the interpolation is performed
+
+    Returns:
+        a numpy array of shape (N, 4, 4) representing N affine transforms
+    '''
+    # Interpolate translation vectors
     interp_func = scipy.interpolate.interp1d(
             np.array([0.0, 1.0]),
             np.vstack((translation_start, translation_end)),
             axis=0)
     translations = interp_func(samples)
 
+    # Create identity affine transforms
     translation_matrices = np.tile(np.eye(4), np.array([len(samples), 1, 1]))
+
+    # Set translation components
     translation_matrices[:,0:3,3] = translations
 
     return translation_matrices
 
 def process_scan(scan, tf_start, tf_end, settings):
+    '''
+    Do all processing necessary to detect obstacles from a scan and publish
+    them
+    '''
     ranges = np.array(scan.ranges)
     angles = np.linspace(scan.angle_min, scan.angle_max, len(ranges))
 
+    # Mask out invalid points
     range_mask = ((ranges > scan.range_min)
                 & (ranges < scan.range_max)
                 & (ranges >= settings['drone_radius']))
@@ -160,6 +263,7 @@ def process_scan(scan, tf_start, tf_end, settings):
     valid_angles = angles[range_mask]
     samples = np.linspace(0.0, 1.0, len(ranges))[range_mask]
 
+    # Convert ranges to vectors
     valid_range_points = valid_ranges * np.array([np.cos(valid_angles),
                                                   np.sin(valid_angles),
                                                   np.zeros(valid_angles.shape)])
@@ -170,14 +274,17 @@ def process_scan(scan, tf_start, tf_end, settings):
 
     assert valid_range_points.shape == (len(valid_ranges), 4)
 
+    # Get interpolated transforms
     transform_matrices = get_transform_matrices(tf_start, tf_end, samples)
 
     assert transform_matrices.shape == (len(valid_ranges), 4, 4)
 
+    # Transform all points into map frame
     transformed_range_points = np.einsum('tij,tj->ti',
                                          transform_matrices,
                                          valid_range_points)
 
+    # Mask out points outside arena bounding box
     mask = ((transformed_range_points[:,0] >= settings['min_x'])
           & (transformed_range_points[:,0] <= settings['max_x'])
           & (transformed_range_points[:,1] >= settings['min_y'])
@@ -185,8 +292,10 @@ def process_scan(scan, tf_start, tf_end, settings):
           & (transformed_range_points[:,2] >= settings['min_z'])
           & (transformed_range_points[:,2] <= settings['max_z']))
 
+    # Consider only (x, y) component of points
     points = transformed_range_points[mask][:,:2]
 
+    # Publish empty obstacle list if there are no points
     if not points.size:
         msg = ObstacleArray()
         msg.header.stamp = scan.header.stamp
@@ -194,6 +303,7 @@ def process_scan(scan, tf_start, tf_end, settings):
         rospy.loginfo('No valid points in laser scan, returning')
         return
 
+    # Group close-together points into clusters
     clusters = [[points[0]]]
     for i in xrange(1, len(points)):
         if (linalg.norm(points[i] - points[i-1])
@@ -208,22 +318,22 @@ def process_scan(scan, tf_start, tf_end, settings):
                 < settings['cluster_threshold']):
         clusters[0].extend(clusters.pop())
 
+    # Estimate positions of clusters
     msg = ObstacleArray()
     msg.header.stamp = scan.header.stamp
     marker_msg = MarkerArray()
+
     arena_center = np.array((settings['arena_center_x'],
                              settings['arena_center_y']))
-
     drone_pos = np.einsum('ij,j->i',
                           transform_matrices[0],
                           np.array((0.0, 0.0, 0.0, 1.0)))[:2]
 
     for cluster in clusters:
         cluster_points = np.stack(cluster, axis=0)
-        obst_pos = get_circle(cluster_points,
-                              settings['obst_radius'],
-                              drone_pos,
-                              settings)
+        obst_pos = get_best_circle_for_cluster(cluster_points,
+                                               settings['obst_radius'],
+                                               drone_pos)
 
         obst_vel = np.cross(np.concatenate((obst_pos - arena_center,
                                             np.array((0,)))),
