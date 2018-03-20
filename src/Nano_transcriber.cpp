cdf3bc6b8edfd5c6fed57756d26e5d61128a516c
pittras <ras@pitt.edu>
2018-03-20 06:08:38 -0400

    Make small adjustments for 1.5

--- src/Nano_transcriber.cpp
+++ src/Nano_transcriber.cpp
@@ -40,33 +40,39 @@ void distributeMessages(iarc7_msgs::Nano nano_info,
                         ros::Publisher& rangefinder_pub,
                         ros::Publisher& long_range_pub){
 
-    ROS_WARN("We are distributing things - current time - %f", ros::Time::now().toSec());
+//  ROS_WARN("We are distributing things - current time - %f", ros::Time::now().toSec());
     sensor_msgs::Range long_range_msg;
     sensor_msgs::Range short_range_msg;
     iarc7_msgs::FlowVector flow_msg;
 
     if(nano_info.identifier == 1){
-        ROS_WARN("Got a short range message");
+//        ROS_WARN("Got a short range message");
         short_range_msg.range = nano_info.short_range;
         short_range_msg.min_range = 0.01;
         short_range_msg.max_range = 0.800;
         short_range_msg.header.stamp = nano_info.msg_received;
+        short_range_msg.radiation_type = sensor_msgs::Range::INFRARED;
+        short_range_msg.field_of_view = 0.3;
+        short_range_msg.header.frame_id = "/short_distance_lidar";
         rangefinder_pub.publish(short_range_msg);
         return;
     }
 
     else if(nano_info.identifier == 2){
-        ROS_WARN("Got a long range message");
+ //       ROS_WARN("Got a long range message");
         long_range_msg.range = nano_info.long_range;
         long_range_msg.min_range = 0.3;
         long_range_msg.max_range = 10;
         long_range_msg.header.stamp = nano_info.msg_received;
+        long_range_msg.radiation_type = sensor_msgs::Range::INFRARED;
+        long_range_msg.field_of_view = 0.3;
+        long_range_msg.header.frame_id = "/tfmini";
         long_range_pub.publish(long_range_msg);
         return;
     }
 
     else if(nano_info.identifier == 4){
-        ROS_WARN("Got an optical flow message");
+ //       ROS_WARN("Got an optical flow message");
         flow_msg.header.stamp = nano_info.msg_received;
         flow_msg.deltaX = nano_info.deltaX;
         flow_msg.deltaY = nano_info.deltaY;
